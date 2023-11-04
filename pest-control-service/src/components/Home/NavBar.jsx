a2d3ed32aa6a594879ae1e1450097e1630e6d8d6
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-04 22:31:42 +0600

    added login and signup page

--- pest-control-service/src/components/Home/NavBar.jsx
+++ pest-control-service/src/components/Home/NavBar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  // Assuming you have a state to track whether the user is logged in or not
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
@@ -15,7 +19,6 @@ const NavBar = () => {
                 src="https://i.ibb.co/MBt7ntK/c02a0527874bb2d01be8ea75f1346c51.jpg"
                 alt=""
               />
-
               <h2 className="text-white font-extrabold text-2xl ml-3">
                 Pest <span className="text-green-500">Control</span>
               </h2>
@@ -26,31 +29,39 @@ const NavBar = () => {
             className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
           >
             <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
-              <a
-                className="font-medium text-blue-500"
-                href="#"
-                aria-current="page"
-              >
-                Landing
-              </a>
-              <a
+              <Link to="/" className="font-medium text-blue-500">
+                Home
+              </Link>
+              <Link
+                to="/services"
                 className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
               >
-                Account
-              </a>
-              <a
-                className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
-              >
-                Work
-              </a>
-              <a
-                className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
-                href="#"
-              >
-                Blog
-              </a>
+                Services
+              </Link>
+              {isLoggedIn ? (
+                <>
+                  <Link
+                    to="/dashboard"
+                    className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                  >
+                    Dashboard
+                  </Link>
+                  <a
+                    onClick={() => setIsLoggedIn(false)}
+                    className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                    href="#"
+                  >
+                    Logout
+                  </a>
+                </>
+              ) : (
+                <Link
+                  to="/login"
+                  className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+                >
+                  Log in
+                </Link>
+              )}
             </div>
           </div>
         </nav>
--- pest-control-service/src/components/Routes/Routes.jsx
+++ pest-control-service/src/components/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../../Root/Root";
 import Homepage from "../Home/Homepage";
 import ErrorPage from "../Error/ErrorPage";
+import LogIn from "../Pages/Login/LogIn";
+import SignUp from "../Pages/SignUp/SignUp";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -21,6 +23,14 @@ const router = createBrowserRouter([
       {
         path: "/service/:id",
         // element: <SingleServicePage />,
+      },
+      {
+        path: "login",
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "signIn",
+        element: <SignUp></SignUp>,
       },
     ],
   },
