d4bf339d555555a5b3798d627f9efd235c2a0226
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-05 12:51:44 +0600

    fix bashboard and mobile responsiveness

--- pest-control-service/src/components/Home/NavBar.jsx
+++ pest-control-service/src/components/Home/NavBar.jsx
@@ -4,10 +4,11 @@ import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDashboardOpen, setIsDashboardOpen] = useState(false);
+
   const { user, logOut } = useContext(AuthContext);
   const location = useLocation();
 
+  // logout function
   const handleLogOut = () => {
     logOut().then((result) => {
       console.log(result);
@@ -18,32 +19,54 @@ const NavBar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const toggleDashboardMenu = () => {
-    setIsDashboardOpen(!isDashboardOpen);
-  };
-
   return (
     <div>
-      <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <header className="flex  dark:bg-gray-800 ">
         <nav
-          className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+          className="max-w-[80rem] w-full mx-auto px-4 sm:flex sm:justify-between"
           aria-label="Global"
         >
-          <NavLink to="/">{/* ... */}</NavLink>
-          <div className="flex items-center">
-            <div className="sm:hidden">
+          <NavLink to="/">
+            {
+              <div className="flex flex-col lg:flex-row justify-center items-center">
+                <img
+                  className="w-[70px] mr-4"
+                  src="https://i.ibb.co/MBt7ntK/c02a0527874bb2d01be8ea75f1346c51.jpg"
+                />
+                <h2 className="text-xl md:text-2xl text-white lg:text-5xl font-bold ">
+                  Pest <br />
+                  <span className="text-green-400">Control</span>
+                </h2>
+              </div>
+            }
+          </NavLink>
+          <div className="flex justify-center items-center lg:flex-col-reverse">
+            <div className="sm:hidden flex flex-row">
               <button
                 onClick={toggleMenu}
                 type="button"
                 className="text-gray-500 hover:text-gray-700 focus:outline-none"
               >
-                {/* ... */}
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
               </button>
             </div>
             <div
               className={`${
                 isMenuOpen ? "block" : "hidden"
-              } sm:flex sm:items-center sm:ml-4`}
+              } sm:flex flex-row sm:items-center sm:ml-4`}
             >
               <NavLink
                 exact
@@ -63,42 +86,61 @@ const NavBar = () => {
               </NavLink>
               {user ? (
                 <>
-                  <div
-                    className="relative"
-                    onMouseEnter={toggleDashboardMenu}
-                    onMouseLeave={toggleDashboardMenu}
+                  {/* drop down dashboard */}
+                  <NavLink
+                    className={`ml-4 font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 ${
+                      location.pathname === "/dashboard" ? "underline " : ""
+                    }`}
                   >
-                    <NavLink
-                      to="/dashboard"
-                      className={`ml-4 font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 ${
-                        location.pathname === "/dashboard" ? "underline " : ""
-                      }`}
-                    >
-                      Dashboard
-                    </NavLink>
-                    {isDashboardOpen && (
-                      <div className="absolute mt-2 py-2 w-48 bg-white border border-gray-300 dark:border-gray-700 dark:bg-gray-800 text-gray-700 dark:text-gray-300">
-                        <NavLink
-                          to="/dashboard/my-services"
-                          className="block px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700"
+                    <div className="hs-dropdown relative inline-flex [--trigger:hover]">
+                      <button
+                        id="hs-dropdown-hover-event"
+                        type="button"
+                        className="hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 "
+                      >
+                        Dashboard
+                        <svg
+                          className="hs-dropdown-open:rotate-180 w-2.5 h-2.5 text-gray-600"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 16 16"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
                         >
-                          My Services
-                        </NavLink>
-                        <NavLink
-                          to="/dashboard/add-services"
-                          className="block px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700"
+                          <path
+                            d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                          />
+                        </svg>
+                      </button>
+
+                      <div
+                        className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:left-0 after:w-full before:h-4 before:absolute before:-top-4 before:left-0 before:w-full"
+                        aria-labelledby="hs-dropdown-hover-event"
+                      >
+                        <a
+                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                          href="#"
                         >
-                          Add Services
-                        </NavLink>
-                        <NavLink
-                          to="/dashboard/my-schedules"
-                          className="block px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700"
+                          My-services
+                        </a>
+                        <a
+                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                          href="#"
                         >
-                          My Schedules
-                        </NavLink>
+                          Add-services
+                        </a>
+                        <a
+                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                          href="#"
+                        >
+                          My-schedules
+                        </a>
                       </div>
-                    )}
-                  </div>
+                    </div>
+                  </NavLink>
                   <button
                     onClick={handleLogOut}
                     className="btn btn-accent ml-4 font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
