3dfb7cf13f3c8a14fbc6bc22f0c775b63e67bf57
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-12-16 19:02:36 +0600

    change the add button color tored

--- pest-control-service/src/components/Pages/AddService/AddService.jsx
+++ pest-control-service/src/components/Pages/AddService/AddService.jsx
@@ -206,7 +206,7 @@ const AddService = () => {
                   <button
                     //   onClick={handleAdd()}
                     type="submit"
-                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-primary-800"
                   >
                     Add
                   </button>
